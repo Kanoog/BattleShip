18fd936d067a9f28214adb8b6df3c9314d44cfad
Kanoog <49658415+Kanoog@users.noreply.github.com>
2022-12-21 20:52:17 -0600

    Add files via upload

--- BattleShip/src/Ship.java
+++ BattleShip/src/Ship.java
@@ -1,20 +1,25 @@
 /**
  * 
- * @author kanoog
+ * @author Kanoog Moua and Adam Moua
  *
  */
-
-
-
-
 public class Ship {
 	
+	/**
+	 * The attributes of the ship class including the name of the ship,
+	 * the size of the ship, it's life status, how many times it has
+	 * been hit, and the coordinates of it's placement on the board
+	 */
 	private int size;
 	private String name;
 	private boolean isAlive;
 	private int shipHit = 0;
 	private int[][] shipCoords; 
 	
+	/**
+	 * @param takes in the size, name, and coordinates of the ship
+	 * and initializes them.
+	 */
 	public Ship(int size, String name, int[][] array) {
 		this.size = size;
 		this.name = name;
@@ -22,18 +27,30 @@ public class Ship {
 		this.shipCoords = array;
 	}
 	
+	/**
+	 * return the size of the ship
+	 */
 	public int getSize() {
 		return this.size;
 	}
 	
+	/**
+	 * returns the name of the ship
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
-	public boolean isShipAlive() { // returns the status of if the ship is alive
+	/**
+	 * returns the life status of the ship
+	 */
+	public boolean isShipAlive() { 
 		return this.isAlive;
 	}
 	
+	/**
+	 * Check the player's guess to see it has hit the ship
+	 */
 	public boolean checkShip(int checkX, int checkY) {
 		
 		int x = 0;
@@ -48,19 +65,17 @@ public class Ship {
 				}
 			}
 			
-//			System.out.println("Current coordinate " + x + " , " + y);
-			
 			if(x == checkX && y == checkY) {
-				
-//				System.out.println("HIT");
 				shipGotHit();
 				return true;
 			}
 		}
-		return false;
-		
+		return false;	
 	}
 	
+	/**
+	 * Increase the number of hits the ship has received
+	 */
 	public void shipGotHit() {
 		if(this.shipHit == (size-1)) {
 			killShip();
@@ -70,36 +85,14 @@ public class Ship {
 		System.out.println("The ship was hit: " + shipHit);
 	}
 	
+	/**
+	 * Kill off the ship because the x,y
+	 * coordinate(s) of this has been guessed already
+	 */
 	public void killShip() {
 		this.isAlive = false;
 		
 		System.out.println("THIS SHIP IS DEAD");
 	}
-	
-	
-	public static void main(String args[]) {
-		
-		int[][] array = {{3,6},{3,7},{3,8}};
-		
-		
-		
-		Ship ship = new Ship(3, "ship1", array);
-		
-		boolean hit = ship.checkShip(3, 7);
-		
-		System.out.println("Is the ship still alive? " + ship.isShipAlive());
-		
-		boolean hit2 = ship.checkShip(3, 6);
-		System.out.println("Is the ship still alive? " + ship.isShipAlive());
-		boolean hit3 = ship.checkShip(3, 8);
-		System.out.println("Is the ship still alive? " + ship.isShipAlive());
-		
-		
-//		System.out.println(hit);
-		
-		
-	}
-	
-	
 	
 }
